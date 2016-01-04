5cee48474c35ba0740071bd448d569236f4ec32c
Corentin Filoche <corentinfiloche@hotmail.fr>
2016-01-04 23:07:07 +0100

    fin des ajouts

--- SQL/BDA.sql
+++ SQL/BDA.sql
@@ -10,8 +10,7 @@ DROP TYPE BDA_adresse;
 CREATE TYPE BDA_adresse AS OBJECT(
   rue VARCHAR2(50),
   ville VARCHAR2(50),
-  codePostal CHAR(5),
-  departement CHAR(2));
+  codePostal CHAR(5));
 -- CREATION DE L'OBJET ETUDIANT
 CREATE TABLE BDA_etudiant(
   id_etudiant INTEGER NOT NULL PRIMARY KEY, 
@@ -55,7 +54,40 @@ INSERT INTO BDA_ETUDIANT VALUES
 (9, 'Clovis', 'France', 1, TO_DATE('08/01/1206', 'dd/mm/yyyy'));
 INSERT INTO BDA_ETUDIANT VALUES
 (10, 'Céline', 'Dion', 1, TO_DATE('15/04/1962', 'dd/mm/yyyy'));
-
+---------------------------------------------------------------
+INSERT INTO BDA_ENTREPRISE VALUES
+(1, 'Air France', BDA_adresse('rue des papyrus', 'Paris', '75000'));
+INSERT INTO BDA_ENTREPRISE VALUES
+(2, 'Blizzard', BDA_adresse('rue des zerglings', 'Metz', '57000'));
+INSERT INTO BDA_ENTREPRISE VALUES
+(3, 'Nvidia', BDA_adresse('rue des oracles', 'Toulouse', '31200'));
+INSERT INTO BDA_ENTREPRISE VALUES
+(4, 'Carrefour', BDA_adresse('rue des ultralisk', 'Brest', '29120'));
+INSERT INTO BDA_ENTREPRISE VALUES
+(5, 'Alcoliques Anonymes', BDA_adresse('rue de ggboum', 'Bordeaux', '14230'));
+INSERT INTO BDA_ENTREPRISE VALUES
+(6, 'Darty', BDA_adresse('rue des pokémons', 'Lille', '59201'));
+INSERT INTO BDA_ENTREPRISE VALUES
+(7, 'King Jouet', BDA_adresse('rue des pyramides', 'Epinal', '88210'));
+INSERT INTO BDA_ENTREPRISE VALUES
+(8, 'Babou', BDA_adresse('rue des nénuphars', 'Marseille', '13000'));
+INSERT INTO BDA_ENTREPRISE VALUES
+(9, 'La Redoute', BDA_adresse('rue des éléphants', 'Paris', '75520'));
+INSERT INTO BDA_ENTREPRISE VALUES
+(10, 'Peugeot', BDA_adresse('rue des abeilles', 'Pau', '23100'));
+---------------------------------------------------------------
+INSERT INTO BDA_STAGE VALUES
+(1, 1, 1, TO_DATE('08/04/1965', 'dd/mm/yyyy'), TO_DATE('30/06/1965', 'dd/mm/yyyy'), 
+BDA_adresse('rue des anglais', 'Orford', '1000'));
+INSERT INTO BDA_STAGE VALUES
+(2, 3, 7, TO_DATE('01/04/2010', 'dd/mm/yyyy'), TO_DATE('01/09/2010', 'dd/mm/yyyy'), 
+BDA_adresse('rue des francais', 'Grenoble', '39120'));
+INSERT INTO BDA_STAGE VALUES
+(3, 4, 9,  TO_DATE('20/04/2013', 'dd/mm/yyyy'),  TO_DATE('10/09/2013', 'dd/mm/yyyy'), 
+BDA_adresse('rue des germains', 'Lille', '59632'));
+INSERT INTO BDA_STAGE VALUES
+(4, 6, 2, TO_DATE('12/12/2013', 'dd/mm/yyyy'), TO_DATE('30/06/2015', 'dd/mm/yyyy'), 
+BDA_adresse('rue des fromagers', 'Lorient', '35120'));
 -- ############################################################
 -- FONCTIONS PL/SQL
 -- ############################################################
